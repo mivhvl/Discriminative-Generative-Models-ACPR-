99a0de25c6ffafb2246a115985941cfcbc321ac4
pedro.pereira <pppereira3@gmail.com>
2025-03-08 18:38:51 +0000

    Refactor

--- _model.py
+++ _model.py
@@ -3,6 +3,8 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython import display
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 # Define input image dimensions
 IMG_SHAPE = (128, 128, 3)
@@ -43,39 +45,48 @@ def build_gan():
     generator = build_generator()
     discriminator = build_discriminator()
 
-    discriminator.compile(loss='binary_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5), metrics=['accuracy'])
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    return discriminator, generator, generator_optimizer, discriminator_optimizer
 
-    discriminator.trainable = False
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
 
-    gan_input = keras.Input(shape=(LATENT_DIM,))
-    generated_image = generator(gan_input)
-    gan_output = discriminator(generated_image)
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
 
-    gan = keras.Model(gan_input, gan_output)
-    gan.compile(loss='binary_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5))
-    return discriminator, generator, gan
+def train_step(images, discriminator, generator, discriminator_optimizer, generator_optimizer, batch_size=128):
+    noise = tf.random.normal([batch_size, LATENT_DIM])
 
-def train_gan(dataset, discriminator, generator, gan, epochs=10000, batch_size=128):
+    disc_loss = gen_loss = 0
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      generated_images = generator(noise, training=True)
+
+      real_output = discriminator(images, training=True)
+      fake_output = discriminator(generated_images, training=True)
+
+      gen_loss = generator_loss(fake_output)
+      disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    return disc_loss, gen_loss
+
+def train_gan(dataset, discriminator, generator, discriminator_optimizer, generator_optimizer, epochs=100, batch_size=128):
     half_batch = batch_size // 2
    
     for epoch in range(epochs):
         d_losses = []
         g_losses = []
-        batches = dataset.take(39)
-        for batch in batches:
-            real_images = batch
-            noise = np.random.normal(0, 1, (half_batch, LATENT_DIM))
-            fake_images = generator.predict(noise)
-
-            discriminator.trainable = True
-            d_loss_real = discriminator.train_on_batch(real_images, np.ones((half_batch, 1)))
-            d_loss_fake = discriminator.train_on_batch(fake_images, np.zeros((half_batch, 1)))
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-            d_losses.append(d_loss[0])
-            discriminator.trainable = False
-            noise = np.random.normal(0, 1, (batch_size, LATENT_DIM))
-            g_loss = gan.train_on_batch(noise, np.ones((batch_size, 1)))
-            g_losses.append(g_loss)
+        for batch in dataset:
+            d_loss, g_loss = train_step(batch, discriminator, generator, discriminator_optimizer, generator_optimizer, batch_size=half_batch)
+        display.clear_output(wait=True)
         print(f"Epoch {epoch}, D Loss: {np.mean(d_losses)}, G Loss: {np.mean(g_losses)}")
         generate_and_save_images(epoch, generator)
 
